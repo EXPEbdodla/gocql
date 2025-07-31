6f1ae9003c21093f049471e4aeed6152bb4182d4
Bhargav Dodla <bdodla@expediagroup.com>
2025-07-31 10:47:48 -0700

    feat: streamline connection logging for shard handling

--- scylla.go
+++ scylla.go
@@ -500,17 +500,16 @@ func (p *scyllaConnPicker) Put(conn *Conn) {
 			}
 		}
 	} else {
-		if conn.addr == p.shardAwareAddress {
-			p.conns[shard] = conn
-			p.nrConns++
-			if gocqlDebug {
-				p.logger.Printf("scylla: %s put shard %d connection total: %d missing: %d", p.address, shard, p.nrConns, p.nrShards-p.nrConns)
-			}
+		p.conns[shard] = conn
+		p.nrConns++
+		if gocqlDebug {
+			p.logger.Printf("scylla: %s put shard %d connection total: %d missing: %d", p.address, shard, p.nrConns, p.nrShards-p.nrConns)
+		}
 
-			p.logger.Printf("New Conn: %s: %d/%d %s: %s: %s: %s", p.address, shard, nrShards, conn.addr, p.shardAwareAddress, p.conns[shard].addr, conn.conn)
-		} else {
-			p.logger.Printf("Wrong Conn: %s: %d/%d %s: %s: %s: %s", p.address, shard, nrShards, conn.addr, p.shardAwareAddress, nil, conn.conn)
-			//closeConns(conn)
+		p.logger.Printf("New Conn: %s: %d/%d %s: %s: %s: %s", p.address, shard, nrShards, conn.addr, p.shardAwareAddress, p.conns[shard].addr, conn.conn)
+
+		if conn.addr != p.shardAwareAddress {
+			p.logger.Printf("Wrong Conn: %s: %d/%d %s: %s: %s: %s", p.address, shard, nrShards, conn.addr, p.shardAwareAddress, p.conns[shard].addr, conn.conn)
 		}
 	}
 
