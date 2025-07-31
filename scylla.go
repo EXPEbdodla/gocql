d2004ee3ba4cef31a0c525d0c229dd19168c9b64
Bhargav Dodla <bdodla@expediagroup.com>
2025-07-31 10:44:56 -0700

    feat: disable closing connections on wrong shard handling

--- scylla.go
+++ scylla.go
@@ -510,7 +510,7 @@ func (p *scyllaConnPicker) Put(conn *Conn) {
 			p.logger.Printf("New Conn: %s: %d/%d %s: %s: %s: %s", p.address, shard, nrShards, conn.addr, p.shardAwareAddress, p.conns[shard].addr, conn.conn)
 		} else {
 			p.logger.Printf("Wrong Conn: %s: %d/%d %s: %s: %s: %s", p.address, shard, nrShards, conn.addr, p.shardAwareAddress, nil, conn.conn)
-			closeConns(conn)
+			//closeConns(conn)
 		}
 	}
 
